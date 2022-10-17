200da2c98052fbd5a9f9d7ce9c0d75d812ee7115
Vlad <mrakobes10@mail.ru>
2022-10-17 21:56:59 +0300

    Add server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { engine } = require('express-handlebars');
+
+const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home', {
+      name: 'John'
+    });
+});
+
+app.listen(3000);
